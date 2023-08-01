2d3045d5243e0f12eab7e44a5ab8f2916f564152
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-01 09:44:40 +0800

    new branch

--- web_demo.py
+++ web_demo.py
@@ -1,5 +1,5 @@
 """
-This script refers to the dialogue example of streamlit, the interactive generation code of chatglm2 and transformers. We mainly modified part of the code logic to adapt to the generation of our model.
+Test: This script refers to the dialogue example of streamlit, the interactive generation code of chatglm2 and transformers. We mainly modified part of the code logic to adapt to the generation of our model.
 Please refer to these links below for more information:
     1. streamlit chat example: https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
     2. chatglm2: https://github.com/THUDM/ChatGLM2-6B
