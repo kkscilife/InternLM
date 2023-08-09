d93b57434fc3233e5c393ba992080ba3fc236898
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-09 14:51:48 +0800

    debug: give parm to script

--- ci_scripts/data/tmp.sh
+++ ci_scripts/data/tmp.sh
@@ -3,7 +3,7 @@
 echo $GITHUB_WORKSPACE
 tmpvark=$(echo $GITHUB_WORKSPACE |cut -d '/' -f 1-4)/data
 echo "env info in script"
-sleep 120
+srun -p llm2 --job-name=$1 sleep 120
 echo $tmpvark
 ls -l $tmpvark
 echo "expect is xx/cicd"
