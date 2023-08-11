dc83c8e4c1377f0ea84a283d4ff6f0e4ad6a206b
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-11 13:29:30 +0800

    try set the env in yaml then used in script

--- ci_scripts/data/tmp.sh
+++ ci_scripts/data/tmp.sh
@@ -3,8 +3,9 @@
 echo $GITHUB_WORKSPACE
 tmpvark=$(echo $GITHUB_WORKSPACE |cut -d '/' -f 1-4)/data
 echo "env info in script"
-srun -p llm2 --job-name=$1 sleep 120
-echo $tmpvark
-ls -l $tmpvark
+echo "new_k in from yaml is ${new_k}"
+srun -p ${new_k} --job-name=$1 sleep 120
+#echo $tmpvark
+#ls -l $tmpvark
 echo "expect is xx/cicd"
 
