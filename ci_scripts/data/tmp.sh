8d0adf515663a201d33edad546f031287f60561e
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-11 13:34:32 +0800

    use default variables in script

--- ci_scripts/data/tmp.sh
+++ ci_scripts/data/tmp.sh
@@ -4,7 +4,7 @@ echo $GITHUB_WORKSPACE
 tmpvark=$(echo $GITHUB_WORKSPACE |cut -d '/' -f 1-4)/data
 echo "env info in script"
 echo "new_k in from yaml is ${new_k}"
-srun -p ${new_k} --job-name=$1 sleep 120
+srun -p ${new_k} --job-name=${GITHUB_RUN_ID}-${GITHUB_JOB} sleep 120
 #echo $tmpvark
 #ls -l $tmpvark
 echo "expect is xx/cicd"
